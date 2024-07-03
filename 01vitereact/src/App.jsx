c31b70568edc2ab7d4daafaef329ca3e5bba167f
harsh7913 <workmailharsh@gmail.com>
2024-07-04 05:19:44 +0530

    adding the basic

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Chai from "./chai"
+
+function App() {
+
+  return (
+    <>
+    <h1>Harsh practising react</h1>
+    <p>testing phase</p>
+    <Chai/>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,11 @@
+function Chai(){
+    return (
+        <>
+        <h2>we are ready to rock</h2>
+        <h3>chai is ready</h3>
+        </>
+        
+    )
+}
+
+export default Chai
